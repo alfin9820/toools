ff54818ae1e0e10a351e886574216a801945b271
alfin9820 <62512478+alfin9820@users.noreply.github.com>
2020-03-23 10:14:57 +0700

    tes.sh
    Mboh

---
+++ tes.sh
@@ -0,0 +1,139 @@
+#clear
+
+#Variables
+b="\033[1m"
+u="\033[4m"
+bl="\033[30m"
+r="\033[31m"
+g="\033[32m"
+bu="\033[34m"
+m="\033[35m"
+c="\033[36m"
+w="\033[37m"
+endc="\033[0m"
+enda="\033[0m"
+blue="\033[1;34m"
+cyan="\033[1;36m"
+red="\033[1;31m"
+
+figlet ALFIN | lolcat
+
+echo "_____________________________________________________________"
+echo "Tools    : Kumpulan Tools jahat $white                         " |lolcat
+echo "Creadby  : Mr.cupliz pake z $white                                      " |lolcat
+echo "Contact  : 089507410104   $white                             " |lolcat
+echo "_____________________________________________________________"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cyan"[#]> Thanks"
+sleep 1
+echo ""
+echo $white"[#]> see you gayn :)"
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo $b "1.  Nmap${enda}";
+echo "============================" | lolcat
+echo $r "2.  Admin-finder${endc}";
+echo "============================" | lolcat
+echo $g "3.  RED_HAWK${endc}";
+echo "============================" | lolcat
+echo $c "4   Lazymux${endc}";
+echo "============================" | lolcat
+echo $c "5   dokOTP spam${endc}";
+echo "============================" | lolcat
+echo $r "6.  Tools-X${endc}";
+echo "============================" | lolcat
+echo $r "7. Exit${endc}";
+echo ""
+echo "╭─Mr.CpZ" |lolcat
+read -p "╰─#" pil;
+
+# Nmap
+
+case $pil in
+1) pkg install nmap
+echo "${y} {1} Masukkan Web${endc}:"
+read web
+nmap $web
+echo
+
+;;
+
+# admin-finder
+
+2) git clone  https://github.com/the-c0d3r/admin-finder.git
+echo -e "${y} cara menggunakan admin finder"
+echo -e "${y} cd admin-finder"
+echo -e "${y} python admin-finder.py"
+cd /data/data/com.termux/files/home/admin-finder/
+python2 /data/data/com.termux/files/home/admin-finder/admin-finder.py
+echo
+
+;;
+
+#RED_HAWK
+
+3) git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e "${y} Installer RED_HAWK..."
+echo -e "${y} cd RED_HAWK"
+echo -e "${y} php RED_HAWK.php"
+cd /data/data/com.termux/files/home/RED_HAWK/
+php /data/data/com.termux/files/home/RED_HAWK/ RED_HAWK.php
+
+;;
+
+#Lazymux
+
+4) git clone https://github.com/Gameye98/Lazymux
+echo "${y} Installer Lazymux..."
+echo "${y} cd Lazymux"
+echo "${y} python lazymux.py"
+cd /data/data/com.termux/files/home/Lazymux/
+python2 /data/data/com.termux/files/home/Lazymux/ lazymux.py
+
+;;
+
+#dokOTP
+
+5) git clone https://github.com/KANG-NEWBIE/dokOTP
+echo "${y} cd dokOTP..."
+echo "${y} pip install bs4 requests"
+echo "${y} python haldoc.py"
+cd /data/data/com.termux/files/home/dokOTP/
+python /data/data/com.termux/files/home/dokOTP/ haldoc.py
+
+;;
+
+#Tools-X
+
+6) git clone https://github.com/Rajkumrdusad/Tool-X
+echo "${y} Installer Tool-X..."
+echo "${y} cd Tool-X"
+echo "${y} sh install.aex"
+cd /data/data/com.termux/files/home/Tool-X
+bash /data/data/com.termux/files/home/Tool-X/sh install.aex
+
+;;
+
+
+7) echo "created by : Mr.CpZ GANS" | lolcat
+exit
+;;
+
+*) echo "jangan bodoh seperti mantan q"
+esac
+done
+done
+
